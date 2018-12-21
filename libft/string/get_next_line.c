e981f3bf0cb719fe7b790806f63a4f1974190163
jgroc-de <jgroc-de@student.42.fr>
2018-12-21 14:34:50 +0100

    correction gnl

--- libft/string/get_next_line.c
+++ libft/string/get_next_line.c
@@ -13,95 +13,126 @@
 #include "get_next_line.h"
 #include "../printf/libprintf.h"
 
-static char	*aux_join(char **line, t_list *node)
+char	*aux_join(char **line, t_list *node, int max)
 {
-	int		len;
 	t_list	*tmp;
-	char	*str;
+	int		len;
+	char	*test;
 
-	len = 0;
 	tmp = node;
-	while (tmp)
+	//ft_printf("\n ** copy **\n");
+	//ft_printf("max init: %d\n", max);
+	if (!tmp)
+		return (NULL);
+	if ((test = ft_strchr(tmp->content, '\n')))
 	{
-		*(strchr(tmp->content, '\n')) = '\0';
-		ft_printf("len node: %d\n", strlen(tmp->content));
-		ft_printf("node: %s\n", tmp->content);
-		ft_strcpy(line[len], (char*)tmp->content);
-		if (!tmp->next)
-			break ;
-		len += tmp->content_size;
+		len = test - (char*)tmp->content;
+		//ft_printf("copy \\n: %s---\n", tmp->content);
+		//ft_printf("max: %d\n", max);
+		//ft_printf("len: %d\n", len);
+		(*line)[max] = '\0';
+		max -= len;
+		ft_strncpy(*line + max, tmp->content, len);
+		//ft_printf("line \\n: %s---\n", *line + max);
 		tmp = tmp->next;
 	}
-	str = strchr((char*)tmp->content, '\n');
-	if (!str)
-		str = (char*)tmp->content;
-	str = ft_strcpy((char*)tmp->content, str);
-	return (str);
+	while (tmp)
+	{
+		max -= tmp->content_size;
+		//ft_printf("copy: %s---\n", tmp->content);
+		//ft_printf("max: %d\n", max);
+		ft_strncpy(*line + max, tmp->content, tmp->content_size);
+		//ft_printf("line: %s---\n*\n", *line + max);
+		tmp = tmp->next;
+	}
+	ft_lstdel(&node, &ft_del);
+	//ft_printf("\n ** fin copy **\n");
+	return (*line);
 }
 
-static int		aux_read_loop(t_list **save, char *str,
-		char *save, const int fd)
+int	aux_lstadd(t_list **node, char *save, int *status, int *len)
 {
-	int		status;
-	int		out;
-	int		len;
-	t_list	*node;
+	t_list	*tmp;
+	char	*test;
 
-	status = 1;
-	len = 0;
+	//ft_printf("status: %d\n", *status);
+	//ft_printf("save: %s\n", save);
+	if (!(tmp = ft_lstnew(save, *status + 1)))
+		return (0);
+	((char*)tmp->content)[*status] = '\0';
+	tmp->content_size--;
+	//ft_printf("save: %s\n", tmp->content);
+	ft_lstadd(node, tmp);
+	ft_bzero(save, BUFF_SIZE + 1);
+	if ((test = ft_strchr(tmp->content, '\n')))
+	{
+		*len += test - (char*)tmp->content;
+		//ft_printf("\\n en sortie: %s\n", test + 1);
+		ft_strcpy(save, test + 1);
+		return (2);
+	}
+	else
+	{
+		//ft_printf("\\n nop\n");
+		*len += *status;
+		return (1);
+	}
+}
+
+int		aux_read_loop(t_list **node, char *save, const int fd, int *status)
+{
+	int	len;
+	int	out;
+
 	out = 1;
-	if (*str)
+	len = 0;
+	//ft_printf(" ** read loop ** \n");
+	if (*status > 0)
 	{
-		if (!(node = ft_lstnew(str, ft_strlen(str) + 1)))
+		if (!(out = aux_lstadd(node, save, status, &len)))
 			return (-1);
-		ft_lstadd(save, node);
 	}
-	while (status && out)
+	while (*status && out == 1)
 	{
-		status = read(fd, str, BUFF_SIZE);
-		if (status == -1)
+		if ((*status = read(fd, save, BUFF_SIZE)) == -1)
 			return (-1);
-		if ((*line = ft_strchr(str, '\n')))
-		{
-			**line = '\0';
-
-			out = 1;
-		}
-		if (!(node = ft_lstnew(str, ft_strlen(str) + 1)))
+		if (*status && !(out = aux_lstadd(node, save, status, &len)))
 			return (-1);
-		ft_lstadd(save, node);
-		len += status;
-		ft_printf("\tlen: %d\n", len);
 	}
-	ft_printf("len: %d\n", len - 1);
-	if (!(*line = ft_strnew(sizeof(char) * (len - 1))))
-		return (-1);
-	return (status - 1);
+	save[*status] = '\0';
+	//ft_printf(" ** fin read ** \n");
+	return (len);
 }
 
 int				get_next_line(const int fd, char **line)
 {
-	static char		save[BUFF_SIZE + 1];
-	char			str[BUFF_SIZE + 1];
+	static char		save[BUFF_SIZE + 1];
 	t_list			*node;
+	int				len;
 	int				status;
 
-	(void)save;
+	//ft_printf("---start---\n");
 	node = NULL;
-	*line = save;
-	ft_printf("\nsave: %s\n", save);
+	if (!(status = ft_strlen(save)))
+		status = -1;
+	//ft_printf("\nsave: *%s*\n", save);
+	//ft_printf("save length: %d\n", status);
 	if (fd < 0 || !(line))
 		return (ERROR);
-	if (*line)
-	{
-		ft_strcpy(str, *line);
-		free(*line);
-	}
-	else
-		(str[0] = '\0');
-	if ((status = aux_read_loop(&node, str, save, fd)) == -1)
+	if ((len = aux_read_loop(&node, save, fd, &status)) == -1)
 		return (ERROR);
-	aux_join(line, node);
-	ft_printf("status: %d\n", status);
-	return (status > 1? 1 : 0);
+	if (!(*line = ft_strnew(len)))
+	{
+		ft_lstdel(&node, &ft_del);
+		return (-1);
+	}
+	//set line
+	aux_join(line, node, len);
+	if (len == 0 && status == 0)
+		free(*line);
+	//ft_printf("total length: %d\n", len);
+	//ft_printf("\nsave: *%s*\n", save);
+	//ft_printf("status: %d\n", status);
+	//ft_printf("---end---\n");
+	return (status >= 1? 1 : 0);
 }
